5c11d2b0bc29663dcfe98a5ad096c8b5b89c45b4
Wangatracker <164025430+Wangatracker@users.noreply.github.com>
2025-05-27 14:58:04 +0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjsxzqXwW2PcwowbFSb4jiGTHGv9rdL0w",
+  authDomain: "agent-hive-project.firebaseapp.com",
+  projectId: "agent-hive-project",
+  storageBucket: "agent-hive-project.firebasestorage.app",
+  messagingSenderId: "464476218600",
+  appId: "1:464476218600:web:59aa5ba90c73468d78155d",
+  measurementId: "G-YGQ85D592J"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
