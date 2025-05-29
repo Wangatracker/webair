48393bf6f91edcc9b31ddf35c73d24c557279e19
Wangatracker <164025430+Wangatracker@users.noreply.github.com>
2025-05-29 17:25:45 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyAjsxzqXwW2PcwowbFSb4jiGTHGv9rdL0w",
+  authDomain: "agent-hive-project.firebaseapp.com",
+  projectId: "agent-hive-project",
+  storageBucket: "agent-hive-project.appspot.com",
+  messagingSenderId: "464476218600",
+  appId: "1:464476218600:web:59aa5ba90c73468d78155d",
+  measurementId: "G-YGQ85D592J"
+};
+
+firebase.initializeApp(firebaseConfig);
